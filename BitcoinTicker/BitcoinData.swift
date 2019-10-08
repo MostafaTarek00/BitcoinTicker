286b1abb13980145eda0b0feeec6f631e60953fe
Mostafa <mostafatarek15894@gmail.com>
2019-10-08 14:01:09 -0700

    v1 commit

---
+++ BitcoinTicker/BitcoinData.swift
@@ -0,0 +1,10 @@
+//
+//  BitcoinData.swift
+//  BitcoinTicker
+//
+//  Created by Mustafa on 9/30/19.
+//  Copyright © 2019 Mostafa. All rights reserved.
+//
+
+import Foundation
+
---
+++ BitcoinTicker/BitcoinDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  BitcoinDataModel.swift
+//  BitcoinTicker
+//
+//  Created by Mustafa on 9/30/19.
+//  Copyright © 2019 Mostafa. All rights reserved.
+//
+
+import Foundation
+class BitcoinDataModel {
+    //Declare your model variables here
+    var price : Double = 0
+   
+}
